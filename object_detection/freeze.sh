fc9253c64e4d55804904f1d04047493b05fe82c2
sshleifer <sam@kensho.com>
2017-10-10 21:30:45 -0400

    config eval change

--- object_detection/freeze.sh
+++ object_detection/freeze.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source tensorflow_env.sh
-#export PYTHONPATH="..:./../slim"
+export PYTHONPATH="..:./../slim"
 export PIPELINE_CONFIG_PATH=samples/configs/ssd_mobilenet_v1_kitti.config
-export TRAIN_PATH=kitti_mobilenet/model.ckpt-5\
+
 
 python export_inference_graph.py \
     --input_type image_tensor \
